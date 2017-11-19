212012ff690d1486f602f041e8e2ac72299864d0
Marek Rybakiewicz <mrybak3@uic.edu>
2017-11-19 02:14:37 -0600

    React App

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/**https://gist.github.com/learncodeacademy/25092d8f1daf5e4a6fd3 */
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./index.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      },
+      {
+        test: /\.css$/, 
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, "src"),
+    filename: "index.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
